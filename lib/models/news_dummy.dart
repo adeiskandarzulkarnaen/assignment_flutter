37327aedf40b46f7179527b25646187adb3a2647
Ulpah <2006194>
2023-04-08 15:08:01 +0700

    update

--- lib/models/news_dummy.dart
+++ lib/models/news_dummy.dart
@@ -1,93 +1,132 @@
 import './news.dart';
 
 News hotesNewsDummy = News(
-  id: 'hotes-001', 
-  date: '08-04-2023', 
-  imageUrl: 'https://picsum.photos/1080/690', 
-  title: 'ini Judul berita terbaru',
-  desc: 'ini deskripsi berita',
-  content: 'isi berita', 
+  id: 'hotes-001',
+  date: '08-04-2023',
+  imageUrl:
+      'https://asset.kompas.com/crops/-qVX07s6bRdO2TFZf9kUGDG8tpY=/0x0:0x0/750x500/data/photo/2023/04/08/6430b4290fff2.jpg',
+  title:
+      'Kontroversi Bupati Meranti, Sebut Kemenkeu Isinya Iblis hingga Ingin Daerahnya Dilepas ke Negara Tetangga',
+  desc:
+      'Bupati Kepulauan Meranti Muhammad Adil (tengah) menggunakan rompi tahanan usai terjaring Operasi Tangkap Tangan (OTT) di Gedung Merah Putih KPK, Jakarta, Jumat (7/4/2023). ',
+  content:
+      'Komisi Pemberantasan Korupsi (KPK) menetapkan Bupati Kepulauan Meranti, Riau, Muhammad Adil tersangka penerima dan pemberi suap. Adil ditetapkan sebagai tersangka setelah sebelumnya KPK menciduknya dalam operasi tangkap tangan (OTT) bersama puluhan orang yang terdiri dari pejabat strategis Pemerintah Kabupaten Kepulauan Meranti serta pihak swasta, Kamis (6/4/2023). Total terdapat tiga tersangka yang sudah diumumkan dalam kasus ini. "KPK menetapkan tiga orang tersangka sebagai berikut, Bupati Kepulauan Meranti periode 2021 sampai dengan 2024, MA (Muhammad Adil). Kemudian, FN (Fitria Nengsih) selaku Kepala BPKAD Pemkab Kepulauan Meranti dan MFH (M Fahmi Aressa) selaku Pemeriksa Muda BPK Perwakilan Riau,” kata Wakil Ketua KPK Alexander Marwata dalam konferensi pers di KPK, Jumat (27/4/2023).',
 );
 
 List<News> latesNewsDummy = [
   News(
-    id: 'lates-002', 
-    date: '01-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita', 
-    content: 'isi berita', 
+    id: 'lates-002',
+    date: '01-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/Zy--TSduydh_uXLammybgYLgsTw=/0x0:3360x2240/750x500/data/photo/2023/03/18/6415c8901e4b8.jpg',
+    title: 'Rehan/Lisa Momok Wakil Malaysia, Perkasa Menuju Final',
+    desc:
+        'Rehan Naufal Kusharjanto/Lisa Ayu Kusumawati kalah dari Zhang Si Wei/Huang Ya Qiong dalam semifinal All England 2023 di Utilita Arena Birmingham.',
+    content:
+        'Dari tujuh pertandingan melawan wakil Malaysia, Rehan/Lisa mampu mencatatkan lima kemenangan dan hanya dua kali menelan kekalahan. Catatan tersebut menunjukkan bahwa Rehan/Lisa kerap menjadi momok bagi para ganda campuran Negeri Jiran. Apabila mengacu berdasarkan riwayat pertandingan melawan wakil Malaysia, Rehan/Lisa layak disebut sebagai pasangan yang tangguh. Mereka patut diwaspadai, termasuk oleh Chen Tang Jie/Toh Ee Wei yang akan menjadi lawan pada semifinal Orleans Masters 2023.',
   ),
   News(
-    id: 'lates-002', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita', 
-    content: 'isi berita', 
+    id: 'lates-002',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/m3vdyM_GRtyI5KC9HSCbUa7emm8=/56x0:731x450/750x500/data/photo/2023/04/08/6430cc6ec8a99.jpg',
+    title: 'TPNPB-OPM Tawarkan Negosiasi Damai',
+    desc:
+        'Sudah dua bulan pilot Susi Air berkebangsaan Selandia Baru disandera oleh Tentara Pembebasan Nasional Papua Barat-Organisasi Papua Merdeka (TPNPB-OPM)',
+    content:
+        'Berbarengan dengan itu, negosiasi juga terus dilakukan, kata Kepala Satgas Humas Damai Cartenz, Donny Charles Go. “Negosiasi terus dilakukan oleh pimpinan daerah dengan tim nego. Tim nego sudah punya penawaran sendiri,” kata Donny kepada BBC News Indonesia melalui pesan teks, Jumat (7/4/2023). Tim negosiasi tersebut dikoordinasikan oleh Penjabat Bupati Nduga, Namia Gwijangge. Namun, Donny menegaskan poin-poin penawaran dari tim negosiasi tidak bisa disampaikan secara terbuka. Juru bicara TPNPB-OPM, Sebby Sambom, mengatakan pemerintah Indonesia tidak perlu mengirim pasukannya untuk operasi militer membebaskan pilot. Sebab, kata dia, hal itu justru akan menambah rumit karena potensi korbannya bukan hanya ada di pihak mereka, melainkan juga di pihak TNI/Polri.',
   ),
   News(
-    id: 'lates-003', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-003',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/KEdnVM642_UHBk2SnjjYL4iPWQQ=/0x0:728x486/750x500/data/photo/2018/08/01/2296946672.jpg',
+    title: 'China Tawarkan Penyatuan Secara Damai dengan Taiwan,',
+    desc:
+        'Perdana Menteri China Li Keqiang menjanjikan penyatuan kembali secara damai dengan Taiwan .',
+    content:
+        'Dia juga menanggapi apa yang disebutnya kolusi antara Taipei dan Washington, pendukung dan pemasok senjata internasional utama Taiwan. Pada bulan Agustus, China mengadakan latihan perang di sekitar Taiwan sebagai tanggapan atas kunjungan ke Taipei oleh Ketua DPR AS Nancy Pelosi. Berbicara pada pembukaan pertemuan tahunan parlemen China, Li mengatakan Beijing berdiri dengan prinsip satu China, yang menyatakan bahwa Taiwan adalah bagian dari China, meski tidak secara langsung mengancam aksi militer. "Pemerintah harus menerapkan kebijakan partai kami untuk menyelesaikan masalah Taiwan dan mengambil langkah tegas untuk menentang kemerdekaan Taiwan dan mempromosikan reunifikasi," katanya kepada sekitar 3.000 delegasi di Balai Besar Rakyat Beijing. "Kita harus mempromosikan pembangunan damai hubungan lintas-Selat dan memajukan proses reunifikasi damai China,"',
   ),
   News(
-    id: 'lates-004', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-004',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/JLNpDlOUugYJEhGt8ZJNCdN8Egs=/0x0:0x0/750x500/data/photo/2023/04/07/642ffaa199072.jpg',
+    title:
+        'Kisah Kehidupan Balita di Sicanang, Daerah Penyumbang Stunting Tertinggi di Medan',
+    desc:
+        'Di kontrakan sederhana berukuran 3x8 meter persegi, Veranika (32) tampak ceria menemani anaknya, Elsa (3) dan Cristian Noah (2) bermain kuda-kudaan. Dengan seksama, dia juga mengawasi keduanya agar tidak keluar rumah.',
+    content:
+        'Veranika menyadari pentingnya kesehatan sejak kedua anaknya diagnosis mengalami stunting pada tahun 2022.  Untuk diketahui, kawasan pesisir pantai di Kelurahan Belawan Sicanang, Kecamatan Medan Belawan, Sumatera Utara, tempat Veranika tinggal, menjadi penyumbang angka stunting tertinggi di Kota Medan. Pemukiman kumuh dan miskin menjadi penyebabnya. "Jadi di posyandu tahun lalu, berat Elsa kemarin 8,8 kg dan Noah 7,9 Kg. Badannya tidak sesuai dengan usianya. Pokoknya dia di bawah garis merah, kena stunting," ujar Veranika kepada Kompas.com, di rumahnya, Kamis (6/4/2023). Saat itu kata bidan posyandu, faktor lingkungan dan gizi menjadi penyebab anaknya menderita stunting.',
   ),
   News(
-    id: 'lates-005', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-005',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/w0AKKd1BT9Gve0zVCoRvLrZmIU0=/93x79:921x631/750x500/data/photo/2022/11/26/63818487d64c2.jpg',
+    title:
+        'Cerita Kepala Puskesmas soal Jumlah Dokter Spesialis Anak yang Tak Sebanding dengan Kasus Stunting',
+    desc:
+        'Kepala Puskesmas Sunter Jaya II, Artika T mengungkapkan hal tersulit bagi tenaga kesehatan untuk menekan angka tengkes atau stunting.',
+    content:
+        '"Angka SDM itu yang sedikit. Sementara, ini (balita stunting) lagi banyak-banyak, mau dirujuk-rujukin. Mereka (dokter spesialis anak) kasih jadwalnya cepat. Tapi enggak langsung brek (sekaligus)," tutur Artika. Ahli Gizi Puskesmas Sunter Jaya II, Hillga Tiara Dewi pun meminta agar setiap ibu tidak menyepelekan layanan kesehatan yang diberikan di posyandu. Meski terlihat sepele seperti menimbang berat dan mengukur tinggi anak, nyatanya hal tersebut menjadi salah satu faktor penting untuk melihat gejala stunting pada balita',
   ),
   News(
-    id: 'lates-006', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-006',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/5_eTN0oRvgFT-VVwj1W62ZIF_pc=/0x0:0x0/750x500/data/photo/2023/03/30/642557e82231f.jpg',
+    title:
+        'Ruko-ruko di Pluit yang Kebal Hukum Akhirnya Bakal Dibongkar karena Langgar IMB',
+    desc:
+        'Kondisi terkini ruko-ruko yang berlokasi di Jalan Niaga, Blok Z4 Utara dan Blok Z8 Selatan, Pluit, Penjaringan, Jakarta Utara.',
+    content:
+        'Riang Prasetya berasumsi tentang adanya dugaan bekingan dari pejabat di belakang pemilik ruko. Ia mengatakan, indikasi dugaan permainan atas permasalahan ini ada di oknum Kelurahan Pluit hingga oknum Kecamatan Penjaringan. “Indikasi yang bermain lebih banyak diduga kuat adalah pihak Kecamatan Penjaringan,” ujar Riang kepada Kompas.com pada Rabu (29/3/2023). Riang menyadari bahwa pernyataannya ini sangat tendensius. Tetapi, dugaan adanya permainan ini muncul karena laporan sejak 2019 soal permasalahan tersebut tidak kunjung ditindaklanjuti secara tegas. “Karena ada pembiaran, Jadi, kalau pun saya diundang rapat di tingkat Kelurahan, Kecamatan, atau pun Wali Kota, itu sifatnya seremonial atau basa-basi saja,” kata Riang.',
   ),
   News(
-    id: 'lates-007', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-007',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/2R9ba7ysjpnd1hYsi4zXRvRL60Q=/0x0:0x0/750x500/data/photo/2023/04/05/642d585c282f1.jpg',
+    title:
+        'Polisi Gerebek Rumah Mewah di Duren Sawit, Warga Kaget Ternyata di Dalamnya Ada 20 Orang Diduga WNA',
+    desc:
+        'Sebuah rumah mewah di Jalan Selat Batam, Duren Sawit, Jakarta Timur, digerebek sejumlah anggota polisi.',
+    content:
+        'Tidak lama setelah laki-laki itu menutup telepon, ada banyak mobil langsung memasuki gang rumah Eni. "Mobil yang ketiga datang isinya ternyata rombongan Bareskrim. Mereka turun, terus pakai las punya tukang buat motong gembok pagar rumah mewah itu," tambah Eni. Setelah pagar berhasil dibuka paksa, penyerbuan oleh anggota polisi berlangsung. Beberapa saat kemudian, polisi membawa keluar sekitar 20 orang yang terlihat seperti warga negara asing. Dari situlah Eni dan warga lainnya baru tahu kalau rumah itu diisi oleh cukup banyak orang.',
   ),
   News(
-    id: 'lates-008', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-008',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/44GYS2JOusAQD-K5yPAtJScMOno=/0x0:780x520/750x500/data/photo/2021/04/28/60895e5f48f74.jpg',
+    title:
+        'Terungkapnya 55 WNA Pelaku Penipuan Jaringan Internasional, Markas di Rumah Mewah Terbongkar',
+    desc:
+        'Direktorat Tindak Pidana Umum (Dittipidum) Badan Reserse Kriminal (Bareskrim) Polri mengungkap kasus dugaan penipuan atau fraud telekomunikasi jaringan internasional.',
+    content:
+        '"Dari warga negara asing itu 55, di mana 50 laki-laki dan 5 perempuan," kata Direktur Tindak Pidana Umum Bareskrim Polri Brigjen Djuhandani Rahardjo Puro di Lobi Bareskrim, Mabes Polri, Jakarta, Rabu (5/4/2023). Aksi penipuan dilancarkan dengan sejumlah modus. Salah satunya, menelepon korban dan mengaku-ngaku sebagai polisi setempat. Pelaku kemudian meminta uang kepada para korban. Uang tebusan tersebut dikirimkan ke rekening penampungan yang ada di luar negeri. "Yang dilakukan para pelaku ini semacam kalau di kita menipu dengan telepon, mengaku sebagai polisi. Kadang-kadang minta tebusan, perbuatan seperti itu yang dilakukan," terang Djuhandani.',
   ),
   News(
-    id: 'lates-009', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-009',
+    date: '02-04-2023',
+    imageUrl:
+        'https://asset.kompas.com/crops/f8rvqNbHSEQncqUp9x7Kb2zHe-c=/0x0:0x0/750x500/data/photo/2023/04/04/642c0fe0584e5.jpg',
+    title:
+        'Dukun Pengganda Uang Mbah Slamet Bunuh 12 Orang, Bagaimana agar Kasus Serupa Tak Terulang?',
+    desc:
+        'Tersangka Tohirin (45) alias Mbah Slamet dibawa ke lokasi penemuan mayat diDesa Balun, Kecamatan Wanayasa, Banjarnegara, Jawa Tengah.',
+    content:
+        'Kriminolog dari Universitas Indonesia, Josias Simon, mengatakan penipuan mengatasnamakan penggandaan uang mempunyai pola sejenis. Yakni mengaku diri sebagai kyai, mbah, atau eyang yang menggunakan ilmu pesugihan putih untuk membantu menggandakan modal usaha ataupun membantu yang sedang terlilit utang/keterpukuran ekonomi. Untuk meyakinkan calon korban, pelaku biasanya menambahkan foto uang bergepok-gepok ditambah testimoni keberhasilan uang gaib tersebut. Cara itu dipakai untuk memanipulasi korban agar terlihat berhasil dan akhirnya banyak yang tertarik.',
   ),
   News(
-    id: 'lates-010', 
-    date: '02-04-2023', 
-    imageUrl: 'https://picsum.photos/200', 
-    title: 'judul berita',
-    desc: 'ini deskripsi berita',
-    content: 'isi berita', 
+    id: 'lates-010',
+    date: '02-04-2023',
+    imageUrl: 'https://picsum.photos/200',
+    title: 'Dampak Gempa Bukittinggi, Tebing di Ngarai Sianok Longsor',
+    desc:
+        'Mengakibatkan tebing Ngarai Sianok, Nagari Sianok Anam Suku, Kecamatan IV Koto Kabupaten Agam Sumatera Barat longsor.',
+    content:
+        'Sebelumnya diberitakan Warga Bukittinggi Sumatera Barat berhamburan keluar rumah karena gempa yang mengguncang, Sabtu (8/4/2023) siang. Pusat gempa sendiri di 9 kilometer barat laut dengan kekuatan 4,5 magnitudo. "Gempanya sangat terasa. Semuanya bergetar dan kami melarikan diri keluar rumah agar tidak terimpit jika ada yang runtuh dan jatuh," Alea warga Bukittinggi, Sabtu (8/4/2023) kepada Kompas.com. Alea mengaku bersyukur tidak ada rumahnya terdampak akibat gempa tersebut. "Syukurlah gempanya tidak sampai merusak bangunan rumah," ujarnya. Lebih jauh dikatakan Alea pada Jumat (7/4/2023) malam juga terjadi gempa yang cukup kuat. "Siang ini merupakan gempa yang kedua kalinya. Malam kemarin juga terjadi gempa. Cuman gempa siang ini agak lama dari gempa semalam," ujarnya.',
   ),
 ];
